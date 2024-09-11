94e6ed3056aa0bfbdffcea95eeb36dc30738335c
Francisco <juanfranciscoglezleal@gmail.com>
2024-09-10 21:47:38 -0500

    Avance

---
+++ Clase/objects.js
@@ -0,0 +1,16 @@
+//Objeto declarado sin propiedades
+
+// let persona = {}
+// console.log(persona)
+
+//Objeto persona con propiedades
+
+let persona{
+    nombre: 'Francisco',
+    apellido: 'Gonzalez',
+    edad: 27,
+    estudios: ['TSU','Maestría'],
+    hijos: false
+}
+
+console.log(persona)
