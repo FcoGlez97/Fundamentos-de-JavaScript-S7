21e2ea3e824f57caf820952dea260525f39dc946
Francisco <juanfranciscoglezleal@gmail.com>
2024-09-14 18:59:19 -0500

    Semana 9 finalizada

--- clases.js
+++ clases.js
@@ -36,6 +36,60 @@
 
 //Clases y Herencias
 
+// class Persona{
+//     constructor(nombre, edad){
+//         this.nombre = nombre;
+//         this.edad = edad;
+//     }
+
+//     saludar(){
+//         console.log(`Hola, mi nombre es ${this.nombre} y mi edad es ${this.edad} años`);
+//     }
+
+//     static definicion(){
+//         console.log("Una persona es un ser humano")
+//     }
+
+// }
+
+// class Desarrollador extends Persona{
+
+// }
+
+// Desarrollador.definicion();
+
+
+// Otro ejemplo
+
+// class Persona{
+//     constructor(nombre, edad){
+//         this.nombre = nombre;
+//         this.edad = edad;
+//     }
+
+//     saludar(){
+//         console.log(`Hola, mi nombre es ${this.nombre} y mi edad es ${this.edad} años`);
+//     }
+
+//     static definicion(){
+//         console.log("Una persona es un ser humano")
+//     }
+
+// }
+
+// class Desarrollador extends Persona{
+//     saludoDesarrollador(){
+//         this.saludar();
+//         console.log("Soy un desarrollador de software");
+//     }
+// }
+
+// let desarrolador = new Desarrollador('Francisco',27);
+
+// desarrolador.saludoDesarrollador();
+
+//Palabra reservada SUPER
+
 class Persona{
     constructor(nombre, edad){
         this.nombre = nombre;
@@ -43,7 +97,7 @@ class Persona{
     }
 
     saludar(){
-        console.log(`Hola, mi nombre es ${this.nombre} y mi edad es ${this.edad} años`);
+        console.log(`Hola, mi nombre es ${this.nombre} y mi edad es de ${this.edad} años`);
     }
 
     static definicion(){
@@ -52,6 +106,19 @@ class Persona{
 
 }
 
-Persona.definicion();
+class Desarrollador extends Persona{
+    
+    constructor(nombre, edad, tipo){
+        super(nombre, edad);
+        this.tipo = tipo;
+    }
 
-//Nota: Estamos en vídeo Clase: Clases, minuto 9 con 46 segundos
+    saludoDesarrollador(){
+        this.saludar();
+        console.log(`Soy un ${this.tipo} Developer`);
+    }
+}
+
+let desarrolador = new Desarrollador('Francisco',27, 'Frontend');
+
+desarrolador.saludoDesarrollador();
